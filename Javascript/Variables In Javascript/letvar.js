f19b072a242ecdce23b670d9454bca422b956cca
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-19 16:53:46 +0530

    defining the variable using the let keyword in javascript

---
+++ Javascript/Variables In Javascript/letvar.js
@@ -0,0 +1,9 @@
+//defining the let varibale in javascript
+let a;//varibale declaration
+a = 10;//intialization
+console.log(a);//10
+
+//let a = 20;//CTE: redeclaration not possible
+//but re-assign is possible
+a = 30;
+console.log(a);//30
