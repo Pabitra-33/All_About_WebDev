981e44800f26b2f9227f8b473b06fe3d962472c9
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-23 14:03:17 +0530

    created two variables in javascript

---
+++ Javascript/Variables In Javascript/constVar.js
@@ -0,0 +1,7 @@
+//declaring variables using const keyword
+const x = 5;
+const y = 6;
+const z = x + y;
+
+//displaying it using the html document id
+document.getElementById("display").innerHTML = "The value of z is: "+z;
