3af7d30ab030648ba78854676be42353ac12e359
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-24 15:00:42 +0530

    logic function of sum value declared using var keyword

--- Javascript/Variables In Javascript/varVar.js
+++ Javascript/Variables In Javascript/varVar.js
@@ -1,3 +1,5 @@
-var a;//varibale declaration
-a = 10;//intialization
-console.log(a);//10
+var a = 7;//varibale declaration and initialization
+var b = 6;
+var sum = a + b;
+document.getElementById("testdisplay").innerHTML =
+"The sum of value is: " + sum;
