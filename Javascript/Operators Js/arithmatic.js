bcdca0a6cfc7209a2ec48199ac8f2a55a7b4155c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-11 20:51:57 +0530

    javascript operators

---
+++ Javascript/Operators Js/arithmatic.js
@@ -0,0 +1,12 @@
+function Arithmatic()
+{
+    let a = 5;
+    let b = 10;
+
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(Math.trunc(a/b));//used to eliminate the decimal values and gives number.
+    console.log(a%b);
+}
+Arithmatic();
