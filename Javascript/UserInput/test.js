6ef3f0c3e7ee1d3560cde19f64dcb505e681c63c
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-31 21:05:59 +0530

    testing the javascript input from the user

---
+++ Javascript/UserInput/test.js
@@ -0,0 +1,7 @@
+function input() {
+    let n = prompt("Enter a number: ");
+    //prompt function always takes the input in String format.
+    console.log("value of n is: "+n);
+    console.log(typeof n);
+}
+input();
