24f355739b448e744543d33aad2c0131845b9471
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-11 22:21:27 +0530

    performing the concatenation operator by using external javascript

---
+++ Javascript/Operators Js/concatenation.js
@@ -0,0 +1,12 @@
+function Concatenation(){
+    console.log(1+2+"JSP"+3+4);//3JSP34
+
+    let a = 10;
+    let b = 5;
+    let res = a+b;
+
+    console.log(res);//15
+    console.log("Addition of two numbers is:"+res);
+    console.log(a+" + "+b+" = "+res);
+}
+Concatenation();
