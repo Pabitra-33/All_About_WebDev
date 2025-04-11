f17ae8278360e5c0b81e55fd8f03e257bc2f7edf
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-11 22:36:48 +0530

    performing the string literal of backtick operator where we can write the dynamic expression

---
+++ Javascript/Operators Js/stringliteral.js
@@ -0,0 +1,10 @@
+function main(){
+    let a = 10;
+    let b = 2;
+    let res = a+b;
+
+    let str = `Addition of two numbers is: ${res}`;
+    console.log(str, typeof str);
+    console.log(`${a} + ${b} = ${res}`);
+}
+main();
