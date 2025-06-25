49a6d322a14b00999a5b9cd72b97187120209222
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-25 12:21:29 +0530

    creating the global script file for strict mode

---
+++ Javascript/Strict Mode JS/globaljs.js
@@ -0,0 +1,6 @@
+"use strict"; //this is the global scope strict mode activated
+
+let y = 50; // This is valid
+document.write("The value of the X is: " + y);
+x = 100; // This is not valid
+document.write("The value of the X is: " + x);
