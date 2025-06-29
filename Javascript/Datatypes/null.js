6c6dcb4592507c7c8a1eaf8bec12631fc84115af
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-29 23:41:05 +0530

    made some null datatype change in our code

--- Javascript/Datatypes/null.js
+++ Javascript/Datatypes/null.js
@@ -1,6 +1,7 @@
 function main() {
     let a = null;
+
     console.log(a, typeof a);
     //"typeof" is a special operator used to check the datatype of a variable.
 }
-main();
+main();//function call
