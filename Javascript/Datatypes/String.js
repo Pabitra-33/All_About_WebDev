34ca50259090d75a3fd4d687dc8fb793f0a65972
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-30 22:43:01 +0530

    string datatype javascript file

--- Javascript/Datatypes/String.js
+++ Javascript/Datatypes/String.js
@@ -1,8 +1,8 @@
 function main() {
-    let str = "Java";
+    let str = "Java";//cretaing a string using double quotes
     console.log(str, typeof str);
 
-    let ch = 'J';
+    let ch = 'J';//using single quote
     console.log(ch, typeof ch);
 }
 main();
